ab809f0e6fb7fff1b0f08c7e167728c68908e56f
杨润 <runyang@travelsky.com>
2020-08-11 17:20:03 +0800

    'commit'

--- src/main/java/com/ascii/AsciiBitOperation.java
+++ src/main/java/com/ascii/AsciiBitOperation.java
@@ -19,20 +19,63 @@ public class AsciiBitOperation {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		String str = "qweraq";
+		String str = "ABCA";
 		System.out.println(isUnique(str));
+		System.out.println('A' & 'B');
+		System.out.println(2L & 4L);
+		System.out.println(6L & 8L);
+		System.out.println(14L & 16L);
 	}
 	
+	/**
+	 * 
+	 * @param astr
+	 * @return
+	 */
 	public static boolean isUnique(String astr) {
 		
-		char[] array = astr.toCharArray();
-		for(char c : array) {
-			if(c >= 64) {
-				System.out.println(c);
+		long low64 = 0;
+        long high64 = 0;
+
+        for (char c : astr.toCharArray()) {
+            if (c >= 64) {
+            	// 1L 相当于 long a = 1L ,L表示long 占用8个字节 , 默认为int 所以1L
+            	// 位运算符的优先级低于 + - 相当于 1L << (c - 64)
+                long bitIndex = 1L << c - 64;
+                System.out.println("char = " + c);
+                System.out.println("high64 = " + high64 + ",  \t" + buQuan0(Long.toBinaryString(high64)));
+                System.out.println("bitIndex = " + bitIndex + ",  \t" + buQuan0(Long.toBinaryString(bitIndex)));
+                System.out.print("high64 & bitIndex = ");
+                System.out.println(high64 & bitIndex);
+                System.out.println("-----------");
+                if ((high64 & bitIndex) != 0) {
+                    return false;
+                }
+
+                high64 |= bitIndex;
+            } else {
+                long bitIndex = 1L << c;
+                if ((low64 & bitIndex) != 0) {
+                    return false;
+                }
+
+                low64 |= bitIndex;
+            }
+        }
+        return true;
+	}
+	
+	private static String buQuan0(String str) {
+		int len = str.length();
+		if(len < 8) {
+			String s = "";
+			int l = 8 - str.length();
+			for (int i = 0; i < l; i++) {
+				s += "0";
 			}
+			str = s + str;
 		}
-		
-		return false;
+		return str;
 	}
 
 }
